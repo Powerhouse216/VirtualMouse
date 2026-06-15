4b5c7211059b69ede15e8d42d06b0cd43f50fdc3
Powerhouse216 <innomatteo10@gmail.com>
2026-06-15 12:52:54 +0200

    Deleted VFX

--- MouseJedi/mano.py
+++ MouseJedi/mano.py
@@ -3,105 +3,7 @@ import mediapipe as mp
 import pyautogui
 import numpy as np
 import math 
-import json
-import random
-import sys
-import os
 
-
-#functions
-def resource_path(relative_path):
-    """ Ottieni il percorso assoluto della risorsa, compatibile con PyInstaller """
-    try:
-        # PyInstaller crea una cartella temporanea e memorizza il path in _MEIPASS
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
-    return os.path.join(base_path, relative_path)
-
-def confronta_pose(punti_json, punti_webcam, tipo_mano="Right", soglia_tolleranza=0.4):
-    if not punti_json:
-        return False, 999.0
-    diz_json = {p["id"]: p for p in punti_json}
-    diz_webcam = {p["id"]: p for p in punti_webcam}
-    
-    errore_totale = 0.0
-
-    for i in range(21):
-        pt_j = diz_json[i]
-        pt_w = diz_webcam[i]
-        x_webcam = pt_w["x"]
-        if tipo_mano == "Left":
-            x_webcam = -x_webcam
-
-        distanza = math.sqrt((pt_j["x"] - x_webcam)**2 + (pt_j["y"] - pt_w["y"])**2)
-        errore_totale += distanza
-
-    errore_medio = errore_totale / 21
-    gesto_uguale = errore_medio < soglia_tolleranza
-    
-    return gesto_uguale, errore_medio
-
-def WorldBearer(durata_frame=20):
-    screenshot = pyautogui.screenshot()
-    schermo_base = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
-
-    nome_finestra = "WorldBearing"
-    cv2.namedWindow(nome_finestra, cv2.WND_PROP_FULLSCREEN)
-    cv2.setWindowProperty(nome_finestra, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-    
-    (testo_w, testo_h), _ = cv2.getTextSize("Prepare for destruction", cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
-    testo_x = (larghezzaSchermo - testo_w) / 2
-    testo_y = (altezzaSchermo + testo_h) / 2
-    cv2.putText(schermo_base, "Prepare for destruction", (int(testo_x), int(testo_y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-    
-    for f in range(durata_frame):
-        overlay = schermo_base.copy()
-        num_cubi = random.randint(3, 8) + (f // 2)
-        
-        for _ in range(num_cubi):
-            w = random.randint(40, 150)
-            x = random.randint(0, larghezzaSchermo - w)
-            y = random.randint(0, altezzaSchermo - w)
-            
-            colore = (0, 0, random.randint(100, 160)) if random.random() < 0.6 else (10, 20, random.randint(200, 255))
-            
-            cv2.rectangle(overlay, (x, y), (x + w, y + w), colore, cv2.FILLED)
-            cv2.rectangle(overlay, (x, y), (x + w, y + w), (0, 0, 0), 1)
-
-        cv2.addWeighted(overlay, 0.6, schermo_base, 0.4, 0, schermo_base)
-        cv2.imshow(nome_finestra, schermo_base)
-        
-        if cv2.waitKey(30) & 0xFF == ord('s'):
-            break
-            
-    cv2.destroyWindow(nome_finestra)
-
-def Remembrance(durata_frame=20):
-    nomeF = "Freeze"
-    cv2.namedWindow(nomeF, cv2.WND_PROP_FULLSCREEN)
-    cv2.setWindowProperty(nomeF, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-
-    cv2.waitKey(5)
-    screen = pyautogui.screenshot()
-    sb = cv2.cvtColor(np.array(screen), cv2.COLOR_RGB2BGR)
-    
-    b_base, g_base, r_base = cv2.split(sb)
-    
-    for f in range(durata_frame):
-        fattore_b = f * 5
-        fattore_r = int(f / 5)
-        
-        b = cv2.add(b_base, fattore_b)
-        r = cv2.add(r_base, fattore_r)
-        
-        freezato = cv2.merge([b, g_base, r])
-        cv2.imshow(nomeF, freezato)
-        
-        if cv2.waitKey(30) & 0xFF == ord('s'):
-            break
-            
-    cv2.destroyWindow(nomeF)
 
 #Variable
 
@@ -112,7 +14,6 @@ cliccato = False
 xVecchia, yVecchia = 0, 0
 frenoMovimento = 0.8
 jaring = 30
-nome_file = resource_path("posa_mano_pura.json")
 pyautogui.FAILSAFE = False
 pyautogui.PAUSE = 0
 
@@ -121,13 +22,6 @@ larghezzaSchermo, altezzaSchermo = pyautogui.size()
 mpHands = mp.solutions.hands
 mpDrawing = mp.solutions.drawing_utils
 
-#Json opening
-try:
-    with open(nome_file, "r") as file_json:
-        punti_json = json.load(file_json)
-except FileNotFoundError:
-    print(f"Errore: Il file '{nome_file}' non esiste ancora. Devi prima eseguire lo script che lo crea!")
-    exit()
 
 #hands settings
 hands = mpHands.Hands(
@@ -136,7 +30,7 @@ hands = mpHands.Hands(
     min_detection_confidence=0.7,
     min_tracking_confidence=0.7
 )
-#0 defines the webcam
+#defines the webcam
 cap = cv2.VideoCapture(0)
 print("Premi 'q' per uscire dal programma.")
 
@@ -199,7 +93,7 @@ while cap.isOpened():
             targetX, targetY = int(xStabilizzato), int(yStabilizzato)
             delt = math.hypot(curX - targetX, curY - targetY)
 
-            # 1. Gestione Stato Drag & Click (Gerarchia delle azioni)
+            # 1. Gestione  Drag & Click 
             distanzaDrag = math.hypot(mix - px, miy - py) 
             distanzaClick = math.hypot(px - mx, py - my) 
 
@@ -213,7 +107,7 @@ while cap.isOpened():
                     pyautogui.mouseUp(button='left')
                     isDragging = False
                 
-                # Gestione Click (solo se non trasciniamo)
+                # Gestione Click
                 if distanzaClick < 30:
                     if not cliccato:
                         pyautogui.click()
@@ -231,8 +125,8 @@ while cap.isOpened():
             else:
                 scrollingAttivo = False
 
-            # 3. MOVIMENTO UNIFICATO (La Deadzone applicata qui)
-            # Si muove solo se non stiamo scrollando e se il movimento supera la soglia
+        
+            # Movimento
             if not scrollingAttivo and delt > jaring:
                 pyautogui.moveTo(targetX, targetY)
 
@@ -245,23 +139,7 @@ while cap.isOpened():
             
             base_medio = handLandmarks.landmark[9]
             lunghezza_palmo = math.sqrt((base_medio.x - x_polso)**2 + (base_medio.y - y_polso)**2)
-            
-            datiMano = []
-            for id_punto, landmark in enumerate(handLandmarks.landmark):
-                puntoPerLista = {
-                    "id" : id_punto,
-                    "x" : (landmark.x - x_polso) / lunghezza_palmo,
-                    "y" : (landmark.y - y_polso) / lunghezza_palmo,
-                    "z" : (landmark.z - z_polso) / lunghezza_palmo,
-                }
-                datiMano.append(puntoPerLista)
-            
-            gs, erroreMedio = confronta_pose(punti_json, datiMano, tipo_mano=tipo_mano, soglia_tolleranza=0.4)
-            if gs:
-                if tipo_mano == "Right":
-                    WorldBearer(durata_frame=20)
-                elif tipo_mano == "Left":
-                    Remembrance(durata_frame=20)
+
 
 
             cv2.putText(frame, stato_corrente, (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
